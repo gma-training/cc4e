87bb1a87c56362bc68b107abf72a619b109cc5ca
Graham Ashton <graham@effectif.com>
2024-09-16 10:30:03 +0100

    Exercise 1-11: Update definition of "word"

--- chapter-01/exercise-1-11.c
+++ chapter-01/exercise-1-11.c
@@ -3,6 +3,14 @@
 #define YES 1
 #define NO  0
 
+int is_letter(char c) {
+  return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+int is_digit(char c) {
+  return c >= '0' && c <= '9';
+}
+
 int main() /* count lines, words, chars in input */
 {
   int c, nl, nw, nc, inword;
@@ -13,9 +21,9 @@ int main() /* count lines, words, chars in input */
     ++nc;
     if (c == '\n' )
       ++nl;
-    if (c == ' ' || c == '\n' || c == '\t' )
+    if (inword && ! (is_letter(c) || is_digit(c) || c == '\''))
       inword = NO;
-    else if ( inword == NO ) {
+    else if (inword == NO && is_letter(c)) {
       inword = YES;
       ++nw;
     }
